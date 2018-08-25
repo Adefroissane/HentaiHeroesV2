4f5e1c63791249b1298bb91e47feef2959151949
Adefroissane <adefroissane@hotmail.fr>
2018-08-25 22:53:22 +0200

    " fin event 24/08"

--- src/main/java/AventurePage.java
+++ src/main/java/AventurePage.java
@@ -30,7 +30,7 @@ public class AventurePage extends HentaiHeroesPage {
 
     public LastZonePage openLastZone(WebDriver driver) {
 
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
+        /*WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
         webDriverWait.until(ExpectedConditions.visibilityOf(precedent));
         precedent.click();
 
@@ -39,11 +39,11 @@ public class AventurePage extends HentaiHeroesPage {
         }
         catch(InterruptedException ae){
             ae.printStackTrace();
-        }
+        }*/
 
         WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
-        webDriverWait2.until(ExpectedConditions.visibilityOf(darkLord));
-        darkLord.click();
+        webDriverWait2.until(ExpectedConditions.visibilityOf(lastZone));
+        lastZone.click();
         return new LastZonePage(driver);
     }
 }
