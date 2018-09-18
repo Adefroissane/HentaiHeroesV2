72238427b817577532313a2bb059c645f7077b7a
Adefroissane <adefroissane@hotmail.fr>
2018-09-18 22:52:44 +0200

    "mise a jour defi"

--- src/main/java/TourDeLaGloire.java
+++ src/main/java/TourDeLaGloire.java
@@ -27,7 +27,8 @@ public class TourDeLaGloire extends HentaiHeroesPage {
     @FindBy(xpath = "//*[@id=\"leagues_left\"]/div/div[6]/div/div[2]/span[1]")
     private WebElement ptdeDefi;
 
-    public boolean defiNecessaire(WebDriver driver) {
+    public boolean defiNecessaire(WebDriver driver)
+    {
         WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
         webDriverWait.until(ExpectedConditions.visibilityOf(ptdeDefi));
         if (ptdeDefi.getText().equals("0")) {
@@ -37,13 +38,13 @@ public class TourDeLaGloire extends HentaiHeroesPage {
         }
     }
 
-    public void lancerDefi(WebDriver driver) {
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
-        webDriverWait.until(ExpectedConditions.visibilityOf(defi));
-        if (defi.isDisplayed())
+    public void lancerDefi(WebDriver driver)
+    {
+        boolean defiAFaire = defiNecessaire(driver);
+        if (defiAFaire == true)
         {
-            boolean defiAFaire = defiNecessaire(driver);
-            if (defiAFaire == true) {
+            if (defi.isDisplayed())
+            {
                 defi.click();
                 WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
                 webDriverWait2.until(ExpectedConditions.visibilityOf(affronter));
