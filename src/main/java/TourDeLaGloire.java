0107c817acbf1ee796ea45bc54bea0a801cced96
Adefroissane <adefroissane@hotmail.fr>
2018-09-19 22:22:54 +0200

    "test"

--- src/main/java/TourDeLaGloire.java
+++ src/main/java/TourDeLaGloire.java
@@ -1,9 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class TourDeLaGloire extends HentaiHeroesPage {
 
@@ -24,8 +28,10 @@ public class TourDeLaGloire extends HentaiHeroesPage {
     @FindBy(xpath = "//*[@id=\"battle_win\"]/button")
     private WebElement ok;
 
-    @FindBy(xpath = "//*[@id=\"leagues_left\"]/div/div[6]/div/div[2]/span[1]")
+    @FindBy(xpath = "//*[@id=\"leagues_left\"]/div/div[7]/div/div[2]/span[1]")
     private WebElement ptdeDefi;
+
+
 
     public boolean defiNecessaire(WebDriver driver)
     {
@@ -43,27 +49,46 @@ public class TourDeLaGloire extends HentaiHeroesPage {
         boolean defiAFaire = defiNecessaire(driver);
         if (defiAFaire == true)
         {
-            if (defi.isDisplayed())
-            {
-                defi.click();
-                WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
-                webDriverWait2.until(ExpectedConditions.visibilityOf(affronter));
-                affronter.click();
-                WebDriverWait webDriverWait3 = new WebDriverWait(driver, 5);
-                webDriverWait3.until(ExpectedConditions.visibilityOf(passer));
-                passer.click();
-                WebDriverWait webDriverWait4 = new WebDriverWait(driver, 5);
-                webDriverWait4.until(ExpectedConditions.visibilityOf(ok));
+            List<WebElement> myElements = driver.findElements(By.xpath("//*[@id=\"leagues_middle\"]/div[3]/div[2]/table/tbody/tr[*]/td[4]"));
+            System.out.println("nbre ennemis =" + myElements.size());
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            List<WebElement> combats = null;
+            for (WebElement e : myElements) {
+                js.executeScript("arguments[0].scrollIntoView();", e);
                 try {
-                    Thread.sleep(750);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    Thread.sleep(100);
+                } catch (InterruptedException ab) {
+                    ab.printStackTrace();
                 }
-                ok.click();
-                lancerDefi(driver);
+                System.out.println(e.getText());
+                if (e.getText().equals("0/3") || e.getText().equals("1/3") ||e.getText().equals("2/3"))
+                {
+                    combats.add(e);
+                }
             }
+            js.executeScript("arguments[0].scrollIntoView();", combats.get(1));
+            WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
+            webDriverWait.until(ExpectedConditions.visibilityOf(combats.get(1)));
+            combats.get(1).click();
+
+            WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
+            webDriverWait2.until(ExpectedConditions.visibilityOf(defi));
+            defi.click();
+            WebDriverWait webDriverWait3 = new WebDriverWait(driver, 5);
+            webDriverWait3.until(ExpectedConditions.visibilityOf(affronter));
+            affronter.click();
+            WebDriverWait webDriverWait4 = new WebDriverWait(driver, 5);
+            webDriverWait4.until(ExpectedConditions.visibilityOf(passer));
+            passer.click();
+            WebDriverWait webDriverWait5 = new WebDriverWait(driver, 5);
+            webDriverWait5.until(ExpectedConditions.visibilityOf(ok));
+            try {
+                Thread.sleep(750);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            ok.click();
+            lancerDefi(driver);
         }
     }
-
-
 }
