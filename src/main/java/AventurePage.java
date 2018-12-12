906c8f0f918387fe5b0e9c770334dfe354cc0904
Adefroissane <adefroissane@hotmail.fr>
2018-12-12 18:43:10 +0100

    "event de merde"

--- src/main/java/AventurePage.java
+++ src/main/java/AventurePage.java
@@ -51,8 +51,8 @@ public class AventurePage extends HentaiHeroesPage {
         }
 
         WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
-        webDriverWait2.until(ExpectedConditions.visibilityOf(donatien));
-        donatien.click();
+        webDriverWait2.until(ExpectedConditions.visibilityOf(darkLord));
+        darkLord.click();
         return new LastZonePage(driver);
     }
 }
