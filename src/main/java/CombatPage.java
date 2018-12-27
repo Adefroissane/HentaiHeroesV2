a94edc1f08b35d436a08cc3d89bf1abb18dd7e52
Adefroissane <adefroissane@hotmail.fr>
2018-12-27 08:22:05 +0100

    "retirer passer"

--- src/main/java/CombatPage.java
+++ src/main/java/CombatPage.java
@@ -34,10 +34,10 @@ public class CombatPage extends HentaiHeroesPage{
         WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
         webDriverWait.until(ExpectedConditions.visibilityOf(affronter));
         affronter.click();
-        WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
+        /*WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
         webDriverWait2.until(ExpectedConditions.visibilityOf(passer2));
-        passer2.click();
-        WebDriverWait webDriverWait3 = new WebDriverWait(driver, 5);
+        passer2.click();*/
+        WebDriverWait webDriverWait3 = new WebDriverWait(driver, 10);
         webDriverWait3.until(ExpectedConditions.visibilityOf(oki));
         try{
             Thread.sleep(750);
