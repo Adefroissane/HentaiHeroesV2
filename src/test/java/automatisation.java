0f0ed5974bef7b552772192f142ac418b07cfd12
Adefroissane <adefroissane@hotmail.fr>
2018-12-29 23:15:39 +0100

    "mise a jour defi"

--- src/test/java/automatisation.java
+++ src/test/java/automatisation.java
@@ -19,11 +19,11 @@ public class automatisation {
     @After
     public void teardown()
     {
-        /*try {
-            Thread.sleep(5000);
+        try {
+            Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }
         driver.close();
     }
 
