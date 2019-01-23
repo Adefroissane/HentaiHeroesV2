ba3a79f5b5b1a1772667ca07745268772426d0b1
Adefroissane <adefroissane@hotmail.fr>
2019-01-23 17:12:41 +0100

    "mise a jour defi"

--- src/main/java/TourDeLaGloirePage.java
+++ src/main/java/TourDeLaGloirePage.java
@@ -77,7 +77,7 @@ public class TourDeLaGloirePage extends HentaiHeroesPage {
                 }
             }
             int i = combats.size();
-            int a = i-2;
+            int a = i-1;
             WebElement adversaire = (WebElement) combats.get(a);
             js.executeScript("arguments[0].scrollIntoView();", adversaire);
             WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
