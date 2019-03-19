cc503dffef22ace3edff0d434bccd6ace09ad930
Adefroissane <adefroissane@hotmail.fr>
2019-03-19 02:42:32 +0100

    "mise a jour defi"

--- src/main/java/AventurePage.java
+++ src/main/java/AventurePage.java
@@ -45,7 +45,7 @@ public class AventurePage extends HentaiHeroesPage {
 
     public LastZonePage openLastZone(WebDriver driver) {
 
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
+        /*WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
         webDriverWait.until(ExpectedConditions.visibilityOf(precedent));
         precedent.click();
 
@@ -54,7 +54,7 @@ public class AventurePage extends HentaiHeroesPage {
         }
         catch(InterruptedException ae){
             ae.printStackTrace();
-        }
+        }*/
 
         WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
         webDriverWait2.until(ExpectedConditions.visibilityOf(carole));
